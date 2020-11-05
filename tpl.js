499b0e3bc6c621f5b0cb6567efd6ff4126fead7c
pure <670064326@qq.com>
2020-11-05 10:36:06 +0800

    fix: download tpl

--- tpl.js
+++ tpl.js
@@ -1,6 +1,6 @@
 const url = {
-    react: "direct:git@github.com:pureZjr/z-tpl.git#master",
-    vue: "direct:git@github.com:pureZjr/z-tpl-vue.git#master",
-    miniapp: "direct:git@github.com:pureZjr/z-miniapp-tpl.git#master",
+    react: "direct:https://github.com/pureZjr/z-tpl.git#master",
+    vue: "direct:https://github.com/pureZjr/z-tpl-vue.git#master",
+    miniapp: "direct:https://github.com/pureZjr/z-tpl.git#master",
 };
 module.exports = url;
